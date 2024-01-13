9119da0d176df9484348cd9c1f8f138a04cbcbca
jongumero <jongumero@yahoo.com>
2024-01-13 13:02:24 -0500

    added "show more >" buttons to project cards for descriptions

--- script.js
+++ script.js
@@ -5,4 +5,20 @@ function toggleMenu() {
     icon.classList.toggle("open");
 }
 
+document.querySelectorAll('.show-more-btn').forEach(button => {
+    button.addEventListener('click', function(event) {
+        // Prevent default anchor action
+        event.preventDefault();
 
+        // The .description element is now the next sibling of the clicked button
+        var description = this.nextElementSibling;
+
+        if(description.style.display === "none" || description.style.display === '') {
+            description.style.display = "flex";
+            this.textContent = "Show less <";
+        } else {
+            description.style.display = "none";
+            this.textContent = "Show more >";
+        }
+    });
+});
